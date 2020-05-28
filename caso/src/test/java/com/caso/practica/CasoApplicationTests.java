7f6ff24cefd2640fa8d195cd289dcae3a804293b
betdbc <betcast.25.98@gmail.com>
2020-05-27 23:06:09 -0500

    subiendo proyecto

---
+++ caso/src/test/java/com/caso/practica/CasoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caso.practica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
